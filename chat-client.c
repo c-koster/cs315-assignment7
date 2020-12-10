6c1bdade3f6e12b48240fa3f56a884e2b27f6eaa
c-koster <ckoster@middlebury.edu>
2020-12-10 10:20:35 -0500

    many touches. just need CRTL-D/eof detection and to fix this bug with sending too many bytes

--- chat-client.c
+++ chat-client.c
@@ -12,12 +12,15 @@
 #include <netdb.h>
 #include <string.h>
 #include <pthread.h>
+#include <time.h>
 
 #define BUF_SIZE 4096
 
+#define TIME_BUF 100
+
 typedef struct reciever {
-    int c_fd; 
-} reciever; 
+    int c_fd;
+} reciever;
 
 void *recieve_messages(void *data);
 
@@ -59,7 +62,7 @@ int main(int argc, char *argv[])
 
     // use struct to send conn_fd to child thread
     reciever *new = calloc(1, sizeof(struct reciever));
-    new->c_fd = conn_fd; 
+    new->c_fd = conn_fd;
     if (pthread_create(&child_thread, NULL, recieve_messages, new) < 0) {
             perror("pthread_create");
             exit(11);
@@ -77,21 +80,28 @@ int main(int argc, char *argv[])
 
 
 void *recieve_messages(void *data) {
-
     char buf[BUF_SIZE];
     int n;
-
     reciever *conn_fd = (reciever *) data;
-    
-    while (1) {
-        n = recv(conn_fd->c_fd, buf, BUF_SIZE, 0);
-        buf[n] = '\0';  /* null-terminate string before printing it */
-        puts(buf);
-        memset(buf, 0, BUF_SIZE);
-    }
-    
-    return NULL;
 
+    // timestamp setup
+    char time_string[TIME_BUF];
+    time_t t;
+    struct tm *tmp;
+    t = time(NULL);
+
+    while (1) {
+
+        n = recv(conn_fd->c_fd, buf, BUF_SIZE, 0);
+        //printf("i recieved %d bytes\n",n);
+        buf[n] = '\0';  // null-terminate string before printing
+
+        tmp = localtime(&t);
+        strftime(time_string, TIME_BUF, "%H:%M:%S", tmp);
+        printf("%s: ",time_string);
+        puts(buf);
+    }
+    return NULL;
 }
 
 // name = recv(conn_fd->c_fd, name_buf, BUF_SIZE,0);
@@ -100,5 +110,5 @@ void *recieve_messages(void *data) {
         // name_buf[name] = '\0';
         // msg_buf[msg] = '\0';
         // puts(name_buf);
-        // printf(": "); 
+        // printf(": ");
         // puts(msg_buf);
--- chat-server.c
+++ chat-server.c
@@ -1,17 +1,23 @@
 /*
-* chat-server.c
-* Culton Koster and Victoria Toth
-*/
+ * chat-server.c
+ * Culton Koster and Victoria Toth
+ */
+
+#include <signal.h>
 
 #include <stdlib.h>
+
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+
+#include <unistd.h>
+
+#include <stdio.h>
+#include <string.h>
+
 
 #include <pthread.h>
 
@@ -23,6 +29,7 @@ typedef struct connection {
     struct sockaddr_in sa;
     // these come from the accept statement
     char buf[BUF_SIZE];
+    char buf_out[BUF_SIZE];
     // pass in a buffer so my broadcast_all function can write to threads
     char *conn_remote_ip;
     uint16_t conn_remote_port;
@@ -35,6 +42,7 @@ pthread_mutex_t rubber_duck;
 
 // there is a head node which is globally accessable
 connection *conn_head = NULL;
+char msg_out[BUF_SIZE];
 
 // better be holding the rubber duck when you play wit this gvar (which is used
 // to write formatted strings that broadcast to everyone)
@@ -45,9 +53,12 @@ connection *conn_head = NULL;
 // that I need, and write the input string to all connections.
 
 void *handle_connection(void *data);
+
+// SLL ops
 connection *create_node(int fd, struct sockaddr_in *sa); // create a node
-int broadcast(char *msg, int bytes, char *remote_ip, uint16_t remote_port, char *nickname); // visit every node
+int broadcast(char *msg, int bytes); // visit every node
 int remove_node(int fd); // delete a node from the linked list
+
 
 
 
@@ -95,9 +106,8 @@ int main(int argc, char *argv[])
             exit(15);
         }
 
-        // NOTE
-        conn = create_node(conn_fd,&remote_sa); // this needs rubber duck
-        
+        conn = create_node(conn_fd,&remote_sa);
+
         // put the thread here, and send the fd,SA,buffer into a new thread
         if (pthread_create(&child_thread, NULL, handle_connection, conn) < 0) {
             perror("pthread_create");
@@ -125,6 +135,7 @@ void *handle_connection(void *data)
     char name[BUF_SIZE];
     int new_line;
     char *end_ch = "\0";
+    int n;
 
 
     connection *conn = (connection *) data; // unpack all my variables
@@ -141,11 +152,11 @@ void *handle_connection(void *data)
 
     /* receive and echo data until the other end closes the connection */
     while ((bytes_received = recv(conn->fd, conn->buf, BUF_SIZE, 0)) > 0) {
-        printf(".");
+        //printf(".");
         fflush(stdout);
         //snprintf(conn->buf,BUF_SIZE,"(culton)>%s\n",conn->buf);
-        
-        if( strncmp(conn->buf, "/nick", 5) == 0 ) {
+
+        if ( strncmp(conn->buf, "/nick", 5) == 0 ) {
             name_in = conn->buf + 6;
             strcpy(name, name_in);
             //printf("who here: %s", name);
@@ -153,27 +164,31 @@ void *handle_connection(void *data)
             name[new_line-1] = *end_ch;
             //printf("who here : %s\n", name);
             conn->nickname = name;
-            //printf("name: %s\n", conn->nickname);
+            n = snprintf(conn->buf_out,BUF_SIZE,"User (%s:%d) is now known as %s.\n",  conn->conn_remote_ip, conn->conn_remote_port, conn->nickname);
+            printf("%s\n",conn->buf_out);
+            broadcast(conn->buf_out,n);
+
+        } else {
+            // format a string for printing
+            n = snprintf(conn->buf_out, BUF_SIZE, "%s: %s", (conn->nickname != NULL) ? conn->nickname : "unknown", conn->buf);
+            //printf("sending %d bytes over\n",n);
+            broadcast(conn->buf_out,n);
+
         }
-     
-
-        broadcast(conn->buf,bytes_received, conn->conn_remote_ip, conn->conn_remote_port, conn->nickname);
-
-        //send(conn->fd, conn->buf, bytes_received, 0);
     }
 
-    printf("\n");
-    //snprintf(conn->buf,BUF_SIZE,"(culton)>%s\n",conn->buf);
-    broadcast(conn->buf,bytes_received, conn->conn_remote_ip, conn->conn_remote_port, conn->nickname);
-
-    //remove_node(conn->fd);
+    // if they are out of this loop -- we should disconnect
+    n = snprintf(conn->buf_out, BUF_SIZE, "User %s (%s:%d) has disconnected.", (conn->nickname != NULL) ? conn->nickname : "unknown", conn->conn_remote_ip, conn->conn_remote_port);
+    printf("%s\n",conn->buf_out);
+    broadcast(conn->buf_out,n);
+    remove_node(conn->fd);
 
     // there are two reasons to close a connection
     // when the server quits
-    // when the client sends a disconnect command
 
     return NULL;
 }
+
 
 // singly-linked-list operations -- -- --
 
@@ -197,7 +212,7 @@ connection *create_node(int conn_fd, struct sockaddr_in *remote_sa)
     new->fd = conn_fd;
     new->next = conn_head;
     conn_head = new;
-    
+
     pthread_mutex_unlock(&rubber_duck); // unlock (done adding stuff)
 
     return conn_head; // and return usable connection data
@@ -205,48 +220,82 @@ connection *create_node(int conn_fd, struct sockaddr_in *remote_sa)
 
 
 /*
- * broadcast_all -
- * input:
+ * broadcast_all - should be locked before calling (Before writing a string to the global buffer)
+ * input: a pointer to the string we would like to write to everyone
  * -visit all items in the linked list and write msg to their fd/sockaddr
  * -maybe a file descriptor to ignore (the person who sent it)
  * output: whatever
  */
-int broadcast(char* msg, int bytes, char *remote_ip, uint16_t remote_port, char *nickname)
+int broadcast(char *msg, int bytes)
 {
-    char full_msg[BUF_SIZE];
-
     connection *curr = conn_head;
 
-    if ( nickname != NULL ) {
-        sprintf(full_msg, "%s: %s", nickname, msg);
-    } else {
-        sprintf(full_msg, "%s:%d : %s", remote_ip, remote_port, msg);
-    }
     while (curr != NULL) {
-        
-        send(curr->fd, full_msg, BUF_SIZE, 0);
+        send(curr->fd, msg, bytes, 0);
         curr = curr->next;
     }
-    memset(full_msg, 0, sizeof(full_msg));
+    memset(msg, '\0', BUF_SIZE);
+
     return 0;
 }
 
 /*
  * remove_node -
- * input: a file descriptor by which to
+ * input: a file descriptor by which to locate the node
  * output: 0 on success, -1 if I didn't find anything (error).
  */
 int remove_node(int fd)
 {
-    //connection *curr = conn_head;
+
+    pthread_mutex_lock(&rubber_duck); // this looks like a critical section.
+
+    connection *curr = conn_head;
+    if (curr == NULL) {
+        return -1;
+    }
     // get a pointer to the node with corresponding file descriptor
+    while (curr->fd != fd) {
+        curr = curr->next;
+    }
+
     // if there is none should return -1
 
-    // set previous pointer too curr->next
+    // close the connection
+    //printf("I am removing node %s:%d\n",curr->conn_remote_ip,curr->conn_remote_port);
 
-    // close the connections
-
+    close(curr->fd);
     // and free the memory
-    //close(fd);
+    free(curr);
+    curr = curr->next;
+    // set previous pointer to curr->next
+
+    pthread_mutex_unlock(&rubber_duck); // unlock
+    return 0;
+}
+
+/*
+ * remove_all_nodes - call me when you press control d
+ * input: nothing
+ * -visit all items in the linked list and free/close each
+ * output: whatever, 0?
+ */
+int remove_all_nodes() {
+    char *s = "Connection closed by host.";
+    int s_length = 27;
+    broadcast(s,s_length);
+
+    pthread_mutex_lock(&rubber_duck); // very very critical
+
+    connection *curr = conn_head;
+    connection *temp = conn_head;
+
+    while (curr != NULL) {
+        temp = curr->next; // don't you dare use after free
+        close(curr->fd);
+        free(curr);
+        curr = temp;
+    }
+    pthread_mutex_unlock(&rubber_duck);
+
     return 0;
 }
